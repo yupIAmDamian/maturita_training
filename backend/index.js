588af65669ed7d9608c08f0c2fded18a72c91f2d
Damián František Girašek <86533359+yupIAmDamian@users.noreply.github.com>
2025-01-14 20:57:07 +0100

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+//add dev mode and production mode
+// find more about that
+
+//https://javascript.plainenglish.io/building-rest-api-with-node-express-and-postgresql-28d8cfe30d
+
+//https://snyk.io/blog/preventing-sql-injection-attacks-node-js/
